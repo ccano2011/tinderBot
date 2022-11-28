025e71c89672f2055d29b79971222ff6cfeab54a
Chris Cano <chriscano@Chriss-MacBook-Pro.local>
2022-11-28 06:47:00 -0500

    stashing changes

--- chromedriver-sample-test.py
+++ chromedriver-sample-test.py
@@ -2,7 +2,7 @@ import time
 from selenium import webdriver
 
 driver = webdriver.Chrome('/path/to/chromedriver')  # Optional argument, if not specified will search path.
-driver.get('http://www.pornhub.com/');
+driver.get('http://www.google.com/');
 time.sleep(5) # Let the user actually see something!
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('ChromeDriver')
